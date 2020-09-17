3fb65c9abaec3b06b36fc083ee8333f1b9a862a9
pjaeyoung <saiei2484@gmail.com>
2020-09-17 16:13:41 +0900

    :pencil: chore : css global style * 에 scroll 막기 적용 및 videojs 오타 수정

--- client/src/components/GlobalStyles.js
+++ client/src/components/GlobalStyles.js
@@ -13,14 +13,13 @@ const globalStyles = createGlobalStyle`
         &::-webkit-scrollbar{
             display:none;
         }
+        scrollbar-width:none;
     }
     body{
         font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
         font-size:12px;
         background-color:rgba(20, 20, 20, 1);
         color:white;
-        scrollbar-width : none;
-        
     }
     
 `;
